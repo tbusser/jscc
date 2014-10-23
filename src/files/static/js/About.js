eee65a6f13dfe10dd7950f10f03847774bf85542
Thijs Busser <tbusser@gmail.com>
2014-10-23 11:30:17 +0200

    Add about script
    This commit adds the script which will populate the API list on the
    about page.

--- src/files/static/js/About.js
+++ src/files/static/js/About.js
@@ -5,14 +5,47 @@ require.config({
 	}
 });
 
-require(['Intermediary', 'DataStore', 'ShowHide', 'ScrollTo'], function(Intermediary, DataStore, ShowHide, ScrollTo) {
+require(['Intermediary', 'DataStore', 'ScrollTo'], function(Intermediary, DataStore, ScrollTo) {
 	'use strict';
+
+	function _listApis() {
+		var features = DataStore.getData(),
+		    keys = Object.keys(features),
+		    list = document.createElement('ol'),
+		    index,
+		    ubound;
+
+		list.classList.add('api-list');
+
+		keys.sort(function(a, b) {
+			a = features[a].title.toLowerCase();
+			b = features[b].title.toLowerCase();
+
+			if (a < b) {
+				return -1;
+			} else if (a > b) {
+				return 1;
+			}
+			return 0;
+		});
+
+		for (index = 0, ubound = keys.length; index < ubound; index++) {
+			var feature = keys[index],
+			    data = features[feature],
+			    item = document.createElement('li');
+
+			item.appendChild(document.createTextNode(data.title));
+			list.appendChild(item);
+		}
+
+		outputElem.appendChild(list);
+	}
 
 	function _onDataStoreMessage(event, channel) {
 		Intermediary.unsubscribe('datastore', _subscriptionId);
 		switch (channel) {
 			case 'datastore:download-completed':
-				console.log('about data loaded');
+				_listApis();
 				break;
 			case 'datastore:download-failed':
 			case 'datastore:too-many-attempts':
@@ -23,14 +56,13 @@ require(['Intermediary', 'DataStore', 'ShowHide', 'ScrollTo'], function(Intermed
 				});
 				break;
 		}
-
 	}
 
 	var scrollToController = new ScrollTo(),
 		_subscriptionId = Intermediary.subscribe('datastore', _onDataStoreMessage),
 		outputElem = document.getElementById('api-list');
 
-	scrollToController.init();
-
 	DataStore.loadData();
+
+	scrollToController.init();
 });
