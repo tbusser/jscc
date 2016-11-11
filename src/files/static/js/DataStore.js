ebf7ee93d8c11b9cda59e179beedaa6326d21d2d
Thijs Busser <tbusser@gmail.com>
2016-11-11 09:04:24 +0100

    Fix DataStore for newer Can I Use data
    A problem occurred when trying to load newer Can I Use data which
    contains browsers not present in the additional data set. This was due
    to not checking if the user agent object exists and thus trying to
    access version information of something that wasn't there.

--- src/files/static/js/DataStore.js
+++ src/files/static/js/DataStore.js
@@ -16,10 +16,10 @@
 	'use strict';
 
 	var _agents,
-	    _features,
-	    _caseCount,
-	    _isReady = false,
-	    _rules = {
+	    																				_features,
+	    																				_caseCount,
+	    																				_isReady = false,
+	    																				_rules = {
 		    queryselector             : [/\.querySelector\s*(?:All)*\(/],
 		    getelementsbyclassname    : [/\.getElementsByClassName\s*\(/],
 		    webworkers                : [/new\s*Worker\s*\(/],
@@ -119,7 +119,7 @@
 		    'strict-mode'             : [/(?:\'|")use strict(?:\'|")/],
 		    eventtarget               : [/\.(?:addEventListener|removeEventListener|dispatchEvent)/]
 	    },
-	    _sources;
+	    																				_sources;
 
 	/**
 	* Iterates over the keys of an object and calls a callback function when the
@@ -135,8 +135,8 @@
 
 	function _calculateTotalGlobalUsage(versionList) {
 		var total = 0,
-		    index = 0,
-		    ubound = versionList.length;
+		    																				index = 0,
+		    																				ubound = versionList.length;
 
 		for (; index < ubound; index++) {
 			total += versionList[index].globalUsage;
@@ -188,11 +188,11 @@
 
 	function _createUAVersionObject(versionInfo, agentInfo, key) {
 		var isDisabled = /d/i,
-		    needsPrefix = /x/i,
-		    prefix = false,
-		    noteMatches = versionInfo.support.match(/#(\d+)/),
-		    usage = (isNaN(agentInfo.global_usage) ? 0 : agentInfo.global_usage),
-		    noteLink;
+		    																				needsPrefix = /x/i,
+		    																				prefix = false,
+		    																				noteMatches = versionInfo.support.match(/#(\d+)/),
+		    																				usage = (isNaN(agentInfo.global_usage) ? 0 : agentInfo.global_usage),
+		    																				noteLink;
 
 		if (needsPrefix.test(versionInfo.support)) {
 			prefix = agentInfo.prefix;
@@ -230,8 +230,8 @@
 
 	function _mergeLinks(originalLinks, supplement) {
 		var linkmap,
-		    index,
-		    ubound;
+		    																				index,
+		    																				ubound;
 
 		// Make sure there are supplemental links to go through
 		if (supplement == null || supplement.length === 0) {
@@ -276,7 +276,11 @@
 			merge[agent] = {};
 			// Loop over the array with versions
 			iterate(agentData.version_list, function(version, versionData) {
-				merge[agent][version] = (supportObj[agent][version] == null) ? 'y' : supportObj[agent][version];
+				if (supportObj[agent] === undefined || supportObj[agent][version] === undefined) {
+					merge[agent][version] = 'y';
+				} else {
+					merge[agent][version] = supportObj[agent][version];
+				}
 			});
 		});
 		return merge;
@@ -290,8 +294,8 @@
 			// Iterate over the user agents
 			iterate(data.stats, function(agent, support) {
 				var normalized = [],
-				    currentItem,
-				    previousItem;
+				    																				currentItem,
+				    																				previousItem;
 
 				// Loop over the versions of the current user agent
 				for (var index = 0, ubound = support.length; index < ubound; index++) {
@@ -302,10 +306,10 @@
 					// 3: Try to get the browser information for the version we're
 					//    processing
 					var item = support[index],													/* [1] */
-					    regex = /(?:a|n|p|u|y)/i,
-					    matches = regex.exec(item.support),
-					    supportValue = (matches.length > 0) ? matches[0].toLowerCase() : 'u',	/* [2] */
-					    agentObj = _agents[agent].version_list[item.version];					/* [3] */
+					    																				regex = /(?:a|n|p|u|y)/i,
+					    																				matches = regex.exec(item.support),
+					    																				supportValue = (matches.length > 0) ? matches[0].toLowerCase() : 'u',	/* [2] */
+					    																				agentObj = _agents[agent].version_list[item.version];					/* [3] */
 
 					/*
 					if (!supports[item.support]) {
@@ -356,7 +360,7 @@
 
 	function _normalizeLinks() {
 		var regex = /poly(?:fill)/i,
-		    regexurl = /code.google.com/i;
+		    																				regexurl = /code.google.com/i;
 
 		// Iterate over all features that can be detected
 		iterate(_features, function(feature, data) {
